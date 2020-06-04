7309164f180f43b4477ceb6c6242279749e77638
nidhalbougatf <nidhal.bougatf1@gmail.com>
2020-06-04 13:36:45 +0100

    test moficiation fichier Trainingapplication.java

---
+++ src/main/java/com/doctorif/TrainingApplication.java
@@ -0,0 +1,13 @@
+package com.doctorif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+// commentaire ajouté par nidhal
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+
+}
