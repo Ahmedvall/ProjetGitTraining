d24a45ef46c72cb69913e20e4ce2ea222b3de14d
Ahmed_vall <ahmedvall.bohey@gmail.com>
2020-06-04 13:23:16 +0100

    First Commit

---
+++ Training/src/test/java/com/doctorif/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
